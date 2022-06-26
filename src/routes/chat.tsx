d89bb081e35f1dae771853ea6f0b0609589ac1fd
민경민 <tom.min@zipfund.co>
2022-06-27 01:03:35 +0900

    `roomState`가 null인 경우 홈으로 리다이렉트

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -1,8 +1,20 @@
 import { Flex } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { roomState } from '~/app/atoms/room-state';
+import { ROUTES } from '~/app/configs/app';
 import { MessageForm } from '~/features/chat/components/message-form';
 import { MessageList } from '~/features/chat/components/message-list';
 
 export const ChatPage = () => {
+  const navigate = useNavigate();
+  const room = useRecoilValue(roomState);
+
+  useEffect(() => {
+    if (!room) navigate(ROUTES.HOME, { replace: true });
+  }, [navigate, room]);
+
   return (
     <Flex flexDir='column' h='full'>
       <MessageList />
